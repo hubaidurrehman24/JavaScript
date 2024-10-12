d75bb0eecf8efc8132d2f3f5723a38f0d1e312b2
hubaidurrehman24 <hubaidurrehman03@gmail.com>
2024-10-13 00:10:20 +0500

    javascript

---
+++ DOM/Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,20 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+
+ var randomDiceImage="dice"+randomNumber1 + ".png";
+
+ document.querySelectorAll("img")[0].setAttribute("src","images/"+randomDiceImage);
+
+var randomNumber2=+Math.floor(Math.random()*6)+1;
+ document.querySelectorAll("img")[1].setAttribute("src","images/dice"+ randomNumber2+ ".png");
+
+
+ if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Win"
+ }
+    else if(randomNumber2>randomNumber1){
+        document.querySelector("h1").innerHTML="Player 2 Win"
+      }
+        else{
+            document.querySelector("h1").innerHTML="Draw"
+  }  
+ 
